f77a5cde395abd763d4e3d94cfaa32d9d8d7f0e5
yyt8618 <yyt8618@sina.com>
2017-04-25 14:35:19 +0800

    abc

---
+++ BLL/OffsetValueBLL.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using System;
+using System.Data;
+
+namespace BLL
+{
+    public class OffsetValueBLL
+    {
+        OffsetValueDAL dal = new OffsetValueDAL();
+        NLog.Logger logger = NLog.LogManager.GetLogger("OffsetValueBLL");
+
+        public DataTable GetAllOffsetValue()
+        {
+            try
+            {
+                return dal.GetAllOffsetValue();
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorException("GetAllOffsetValue", ex);
+                return null;
+            }
+        }
+
+        public bool SaveOffsetValue(DataTable dt)
+        {
+            try
+            {
+                return dal.SaveOffsetValue(dt);
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorException("SaveOffsetValue", ex);
+                return false;
+            }
+        }
+    }
+}
